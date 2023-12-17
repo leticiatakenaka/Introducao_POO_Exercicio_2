3300aa863a1932404ed915adc3d0177b78a3c525
Leticia Santos Takenaka <leticiatakenaka12@gmail.com>
2023-12-17 19:33:18 -0300

    Adicionar arquivos de projeto.

---
+++ Introducao_POO_Exercicio_2/Funcionario.cs
@@ -0,0 +1,8 @@
+﻿namespace Introducao_POO_Exercicio_2
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+    }
+}
---
+++ Introducao_POO_Exercicio_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using Introducao_POO_Exercicio_2;
+
+Funcionario f1 = new()
+{
+    Nome = "Carlos Silva",
+    Salario = 6300.00
+};
+
+Funcionario f2 = new()
+{
+    Nome = "Ana Marques",
+    Salario = 6700.00
+};
+
+Console.WriteLine($"Dados do primeiro funcionário:\nNome: {f1.Nome}\nSalário: {f1.Salario}");
+Console.WriteLine($"Dados do segundo funcionário:\nNome: {f2.Nome}\nSalário: {f2.Salario}");
+
+Console.WriteLine($"Salário médio = {(f1.Salario + f2.Salario) / 2}");
